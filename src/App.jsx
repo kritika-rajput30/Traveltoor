420cf0208e27292e6cdfe1ddb80364a29e26c7bb
kritikaVijaysinghRajput <kritikarajput203@gmail.com>
2025-07-25 20:34:35 +0530

    feat:Project Started

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1 className="text-3xl text-blue-500 text-center">Hii</h1>
+      <h1 className="text-3xl text-blue-500 text-center">Hii welcome</h1>
     </div>
   );
 }
